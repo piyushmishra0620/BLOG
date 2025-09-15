4aa8d8ef5560471c11b60d4fe018653d14239760
piyushmishra06 <piyushmishra0620@gmail.com>
2025-09-15 23:15:29 +0530

    New commit

--- frontend/src/Pages/Auth.jsx
+++ frontend/src/Pages/Auth.jsx
@@ -1,3 +1,5 @@
+import {motion,AnimatePresence} from 'framer-motion';
+
 const Auth = () => {
     return (
         <>
@@ -9,8 +11,8 @@ const Auth = () => {
                 <div className="mt-20 flex justify-center items-center min-w-screen min-h-60">
                     <div className="bg-white1 backdrop-blur-[40px] rounded-[10px] p-[40px] max-500w:p-[27px] max-400w:p-[18px] max-330w:p-[12px] border-1 border-white/20 shadow-auth ">
                         <div className="flex flex-col items-center-safe h-fit justify-between gap-2 w-[470px] max-700w:w-[430px] max-600w:w-[370px] max-500w:w-[335px] max-400w:w-[280px] max-330w:w-[220px] mb-10 max-400w:mb-7">
-                            <button className="text-white text-1xl font-sans font-semibold py-4 w-full  rounded-[15px] border-1 border-white/20 cursor-pointer shadow-button">Sign Up</button>
-                            <button className="text-white text-1xl font-sans font-semibold py-4 w-full rounded-[15px] border-1 border-white/20 cursor-pointer shadow-button">Login</button>
+                            <motion.button className="text-white text-1xl font-sans font-semibold py-4 w-full  rounded-[15px] border-1 border-white/20 cursor-pointer shadow-button hover:bg-cyan-300 hover:text-black hover:font-semibold hover:text-1xl hover:font-sans hover:-translate-y-1 duration-200 ease-in-out" >Sign Up</motion.button>
+                            <motion.button className="text-white text-1xl font-sans font-semibold py-4 w-full rounded-[15px] border-1 border-white/20 cursor-pointer shadow-button hover:bg-cyan-300 hover:text-black hover:font-semibold hover:text-1xl hover:font-sans hover:-translate-y-1  duration-200 ease-in-out">Login</motion.button>
                         </div>
                         <div className="flex justify-center items-center-safe w-full mb-10 max-400w:mb-7">
                             <span className="bg-white  w-full h-[0.2px] p-[0.2px] me-1.5"></span>
@@ -23,7 +25,6 @@ const Auth = () => {
                             <button className="text-white text-1xl font-sans font-semibold py-4 w-full rounded-[15px] border-1 border-white/20 cursor-pointer shadow-button">Continue With Apple</button>
                         </div>
                     </div>
-
                 </div>
             </div>
         </>
