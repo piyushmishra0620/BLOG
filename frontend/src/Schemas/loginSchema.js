9704f1bc9efe1264687da78b97009320bb02b597
piyushmishra06 <piyushmishra0620@gmail.com>
2025-10-11 21:03:11 +0530

    Made New Changes

--- frontend/src/Schemas/loginSchema.js
+++ frontend/src/Schemas/loginSchema.js
@@ -1,6 +1,6 @@
 import {z} from 'zod';
 
 export const loginSchema = z.object({
-    email:z.nonempty("Email is required.").email(),
+    email:z.string().nonempty("Email is required.").email(),
     password:z.string().nonempty("Password is required.").min(8,{message:"Password should be a minimum of 8 characters long."})
 });
