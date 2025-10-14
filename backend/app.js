3a49e2fadf03a65c83a70bf82da6c2430a597f60
piyushmishra06 <piyushmishra0620@gmail.com>
2025-10-14 21:08:19 +0530

    Fixed the CORS issue in the backend

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,10 @@ const authroutes = require('./Routes/authRoutes');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    url:"https://blogify-gules-delta.vercel.app",
+    credentials:true
+}));
 app.use(cookieparser());
 
 app.use('/auth',authroutes);
